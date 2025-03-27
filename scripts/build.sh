5e00e4c8688ac5aeadd9d8d5675027e3bcf4fe77
Decio Ferreira <decio.jferreira@gmail.com>
2025-03-27 23:40:15 +0000

    Update README and scripts for performance comparison; adjust HTML formatting

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,7 +22,7 @@ esac
 js="$dir/guida.js"
 min="$dir/guida.min.js"
 
-elm make --optimize --output=$js $elm_entry
+guida make --optimize --output=$js $elm_entry
 
 uglifyjs $js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output $min
 
