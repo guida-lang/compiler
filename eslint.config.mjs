fbc66c6a0150f116258ecfe52f235cad13e74437
Decio Ferreira <decio.jferreira@gmail.com>
2025-04-03 22:59:27 +0100

    Add ESLint testing to CI workflow and update package configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import pluginJest from "eslint-plugin-jest";
 
 
 export default defineConfig([
+  globalIgnores([
+    "bin/guida.js",
+    "bin/guida.min.js",
+    "lib/guida.js",
+    "lib/guida.min.js",
+  ]),
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["bin/**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
