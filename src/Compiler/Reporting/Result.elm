2eda300103a444d11cbeab3928c7747f440d55c2
Decio Ferreira <decio.jferreira@gmail.com>
2025-05-10 00:04:58 +0100

    Refactor traverse function for improved readability and performance

--- src/Compiler/Reporting/Result.elm
+++ src/Compiler/Reporting/Result.elm
@@ -160,41 +160,34 @@ bind callback (RResult ka) =
 
 
 traverse : (a -> RResult i w x b) -> List a -> RResult i w x (List b)
-traverse func list =
-    List.map func list
-        |> List.foldl
-            (\(RResult kv) (RResult acc) ->
-                RResult <|
-                    \i w ->
-                        case acc i w of
-                            ROk i1 w1 accList ->
-                                case kv i1 w1 of
-                                    ROk i2 w2 value ->
-                                        ROk i2 w2 (value :: accList)
+traverse func =
+    List.foldl
+        (\a (RResult acc) ->
+            RResult <|
+                \i w ->
+                    let
+                        (RResult kv) =
+                            func a
+                    in
+                    case acc i w of
+                        ROk i1 w1 accList ->
+                            case kv i1 w1 of
+                                ROk i2 w2 value ->
+                                    ROk i2 w2 (value :: accList)
 
-                                    RErr i2 w2 e2 ->
-                                        RErr i2 w2 e2
+                                RErr i2 w2 e2 ->
+                                    RErr i2 w2 e2
 
-                            RErr i1 w1 e1 ->
-                                case kv i1 w1 of
-                                    ROk i2 w2 _ ->
-                                        RErr i2 w2 e1
+                        RErr i1 w1 e1 ->
+                            case kv i1 w1 of
+                                ROk i2 w2 _ ->
+                                    RErr i2 w2 e1
 
-                                    RErr i2 w2 e2 ->
-                                        RErr i2 w2 (OneOrMore.more e1 e2)
-            )
-            (pure [])
-        |> fmap List.reverse
-
-
-traverseHelp : (a -> RResult i w e b) -> ( List a, List b ) -> RResult i w e (Step ( List a, List b ) (List b))
-traverseHelp callback ( list, result ) =
-    case list of
-        [] ->
-            pure (Done (List.reverse result))
-
-        a :: rest ->
-            fmap (\b -> Loop ( rest, b :: result )) (callback a)
+                                RErr i2 w2 e2 ->
+                                    RErr i2 w2 (OneOrMore.more e1 e2)
+        )
+        (pure [])
+        >> fmap List.reverse
 
 
 mapTraverseWithKey : (k -> comparable) -> (k -> k -> Order) -> (k -> a -> RResult i w x b) -> Dict comparable k a -> RResult i w x (Dict comparable k b)
