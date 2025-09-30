858ae31afa0d2fefcf70a4204cb9b751d440211f
Decio Ferreira <decio.jferreira@gmail.com>
2025-09-30 23:02:39 +0100

    fix format for multi-line record updates

--- tests/format.test.js
+++ tests/format.test.js
@@ -277,6 +277,7 @@ const examples = [
             }
         },
         { title: "multi-line record", filename: "MultiLineRecordDeclarations", module: { ...defaultModule, declarations: [`recordFn =\n { age = 23\n , name =\n "John"\n }`] } },
+        { title: "multi-line update record", filename: "MultiLineUpdateRecordDeclarations", module: { ...defaultModule, declarations: [`updateRecordFn =\n { record | age = 23\n , name = "John"\n }`] } },
     ]],
     // UNION DECLARATIONS
     ["Union", [
@@ -366,11 +367,6 @@ describe("format", () => {
 });
 
 const generateModule = ({ header, docs, imports, infixes, declarations }) => {
-    //     console.log(`${header}
-    // ${docs}
-    // ${imports.join("\n")}
-    // ${infixes.join("\n")}
-    // ${declarations.join("\n")}`);
     return `${header}
 ${docs}
 ${imports.join("\n")}
