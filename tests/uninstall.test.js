942c752ad273dd944f56030db8e7bfeecbb1bc36
Decio Ferreira <decio.jferreira@gmail.com>
2026-01-07 23:15:14 +0000

    added terminal uninstall tests

--- tests/uninstall.test.js
+++ tests/uninstall.test.js
@@ -1,13 +1,321 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const child_process = require("node:child_process");
+const tmp = require("tmp");
+
 describe("guida uninstall command", () => {
-    it.todo("uninstalls elm package on application elm.json");
+    it("uninstalls elm package on application elm.json", () => {
+        const tmpobj = tmp.dirSync();
 
-    it.todo("uninstalls elm package on package elm.json");
+        fs.mkdirSync(path.join(tmpobj.name, "src"));
 
-    it.todo("uninstalls elm package on application guida.json");
+        fs.writeFileSync(path.join(tmpobj.name, "elm.json"), JSON.stringify({
+            type: "application",
+            "source-directories": [
+                "src"
+            ],
+            "elm-version": "0.19.1",
+            dependencies: {
+                direct: {
+                    "elm/browser": "1.0.2",
+                    "elm/core": "1.0.5",
+                    "elm/html": "1.0.1",
+                    "evancz/elm-playground": "1.0.3"
+                },
+                indirect: {
+                    "elm/json": "1.1.4",
+                    "elm/svg": "1.0.1",
+                    "elm/time": "1.0.0",
+                    "elm/url": "1.0.0",
+                    "elm/virtual-dom": "1.0.5"
+                }
+            },
+            "test-dependencies": {
+                direct: {},
+                indirect: {}
+            }
+        }));
 
-    it.todo("uninstalls elm package on package guida.json");
+        child_process.execSync(`${path.join(__dirname, "..", "bin", "index.js")} uninstall evancz/elm-playground --yes`, {
+            cwd: tmpobj.name,
+            stdio: "pipe",
+            env: {
+                ...process.env,
+                GUIDA_HOME: path.join(tmpobj.name, ".guida"),
+                GUIDA_REGISTRY: "http://localhost:3210"
+            }
+        });
 
-    it.todo("uninstalls guida package on application guida.json");
+        expect(fs.existsSync(path.join(tmpobj.name, "elm.json"))).toBe(true);
+        expect(JSON.parse(fs.readFileSync(path.join(tmpobj.name, "elm.json"), "utf-8"))).toEqual({
+            type: "application",
+            "source-directories": [
+                "src"
+            ],
+            "elm-version": "0.19.1",
+            dependencies: {
+                direct: {
+                    "elm/browser": "1.0.2",
+                    "elm/core": "1.0.5",
+                    "elm/html": "1.0.1"
+                },
+                indirect: {
+                    "elm/json": "1.1.4",
+                    "elm/time": "1.0.0",
+                    "elm/url": "1.0.0",
+                    "elm/virtual-dom": "1.0.5"
+                }
+            },
+            "test-dependencies": {
+                direct: {},
+                indirect: {}
+            }
+        });
+    });
 
-    it.todo("uninstalls guida package on package guida.json");
+    it("uninstalls elm package on package elm.json", () => {
+        const tmpobj = tmp.dirSync();
+
+        fs.mkdirSync(path.join(tmpobj.name, "src"));
+
+        fs.writeFileSync(path.join(tmpobj.name, "elm.json"), JSON.stringify({
+            type: "package",
+            name: "author/project",
+            summary: "helpful summary of your project, less than 80 characters",
+            license: "BSD-3-Clause",
+            version: "1.0.0",
+            "exposed-modules": [],
+            "elm-version": "0.19.1 <= v < 1.0.0",
+            dependencies: {
+                "elm/core": "1.0.0 <= v < 2.0.0",
+                "evancz/elm-playground": "1.0.3 <= v < 2.0.0"
+            },
+            "test-dependencies": {}
+        }));
+
+        child_process.execSync(`${path.join(__dirname, "..", "bin", "index.js")} uninstall evancz/elm-playground --yes`, {
+            cwd: tmpobj.name,
+            stdio: "pipe",
+            env: {
+                ...process.env,
+                GUIDA_HOME: path.join(tmpobj.name, ".guida"),
+                GUIDA_REGISTRY: "http://localhost:3210"
+            }
+        });
+
+        expect(fs.existsSync(path.join(tmpobj.name, "elm.json"))).toBe(true);
+        expect(JSON.parse(fs.readFileSync(path.join(tmpobj.name, "elm.json"), "utf-8"))).toEqual({
+            type: "package",
+            name: "author/project",
+            summary: "helpful summary of your project, less than 80 characters",
+            license: "BSD-3-Clause",
+            version: "1.0.0",
+            "exposed-modules": [],
+            "elm-version": "0.19.1 <= v < 1.0.0",
+            dependencies: {
+                "elm/core": "1.0.0 <= v < 2.0.0"
+            },
+            "test-dependencies": {}
+        });
+    });
+
+    it("uninstalls elm package on application guida.json", () => {
+        const tmpobj = tmp.dirSync();
+
+        fs.mkdirSync(path.join(tmpobj.name, "src"));
+
+        fs.writeFileSync(path.join(tmpobj.name, "guida.json"), JSON.stringify({
+            type: "application",
+            "source-directories": [
+                "src"
+            ],
+            "guida-version": "1.0.0",
+            dependencies: {
+                direct: {
+                    "guida-lang/stdlib": "1.0.0",
+                    "evancz/elm-playground": "1.0.3"
+                },
+                indirect: {}
+            },
+            "test-dependencies": {
+                direct: {},
+                indirect: {}
+            }
+        }));
+
+        child_process.execSync(`${path.join(__dirname, "..", "bin", "index.js")} uninstall evancz/elm-playground --yes`, {
+            cwd: tmpobj.name,
+            stdio: "pipe",
+            env: {
+                ...process.env,
+                GUIDA_HOME: path.join(tmpobj.name, ".guida"),
+                GUIDA_REGISTRY: "http://localhost:3210"
+            }
+        });
+
+        expect(fs.existsSync(path.join(tmpobj.name, "guida.json"))).toBe(true);
+        expect(JSON.parse(fs.readFileSync(path.join(tmpobj.name, "guida.json"), "utf-8"))).toEqual({
+            type: "application",
+            "source-directories": [
+                "src"
+            ],
+            "guida-version": "1.0.0",
+            dependencies: {
+                direct: {
+                    "guida-lang/stdlib": "1.0.0"
+                },
+                indirect: {}
+            },
+            "test-dependencies": {
+                direct: {},
+                indirect: {}
+            }
+        });
+    });
+
+    it("uninstalls elm package on package guida.json", () => {
+        const tmpobj = tmp.dirSync();
+
+        fs.mkdirSync(path.join(tmpobj.name, "src"));
+
+        fs.writeFileSync(path.join(tmpobj.name, "guida.json"), JSON.stringify({
+            type: "package",
+            name: "author/project",
+            summary: "helpful summary of your project, less than 80 characters",
+            license: "BSD-3-Clause",
+            version: "1.0.0",
+            "exposed-modules": [],
+            "guida-version": "1.0.0 <= v < 2.0.0",
+            dependencies: {
+                "guida-lang/stdlib": "1.0.0 <= v < 2.0.0",
+                "evancz/elm-playground": "1.0.3 <= v < 2.0.0"
+            },
+            "test-dependencies": {}
+        }));
+
+        child_process.execSync(`${path.join(__dirname, "..", "bin", "index.js")} uninstall evancz/elm-playground --yes`, {
+            cwd: tmpobj.name,
+            stdio: "pipe",
+            env: {
+                ...process.env,
+                GUIDA_HOME: path.join(tmpobj.name, ".guida"),
+                GUIDA_REGISTRY: "http://localhost:3210"
+            }
+        });
+
+        expect(fs.existsSync(path.join(tmpobj.name, "guida.json"))).toBe(true);
+        expect(JSON.parse(fs.readFileSync(path.join(tmpobj.name, "guida.json"), "utf-8"))).toEqual({
+            type: "package",
+            name: "author/project",
+            summary: "helpful summary of your project, less than 80 characters",
+            license: "BSD-3-Clause",
+            version: "1.0.0",
+            "exposed-modules": [],
+            "guida-version": "1.0.0 <= v < 2.0.0",
+            dependencies: {
+                "guida-lang/stdlib": "1.0.0 <= v < 2.0.0"
+            },
+            "test-dependencies": {}
+        });
+    });
+
+    it("uninstalls guida package on application guida.json", () => {
+        const tmpobj = tmp.dirSync();
+
+        fs.mkdirSync(path.join(tmpobj.name, "src"));
+
+        fs.writeFileSync(path.join(tmpobj.name, "guida.json"), JSON.stringify({
+            type: "application",
+            "source-directories": [
+                "src"
+            ],
+            "guida-version": "1.0.0",
+            dependencies: {
+                direct: {
+                    "guida-lang/stdlib": "1.0.0",
+                    "guida-lang/project-metadata-utils": "1.0.0"
+                },
+                indirect: {}
+            },
+            "test-dependencies": {
+                direct: {},
+                indirect: {}
+            }
+        }));
+
+        child_process.execSync(`${path.join(__dirname, "..", "bin", "index.js")} uninstall guida-lang/project-metadata-utils --yes`, {
+            cwd: tmpobj.name,
+            stdio: "pipe",
+            env: {
+                ...process.env,
+                GUIDA_HOME: path.join(tmpobj.name, ".guida"),
+                GUIDA_REGISTRY: "http://localhost:3210"
+            }
+        });
+
+        expect(fs.existsSync(path.join(tmpobj.name, "guida.json"))).toBe(true);
+        expect(JSON.parse(fs.readFileSync(path.join(tmpobj.name, "guida.json"), "utf-8"))).toEqual({
+            type: "application",
+            "source-directories": [
+                "src"
+            ],
+            "guida-version": "1.0.0",
+            dependencies: {
+                direct: {
+                    "guida-lang/stdlib": "1.0.0"
+                },
+                indirect: {}
+            },
+            "test-dependencies": {
+                direct: {},
+                indirect: {}
+            }
+        });
+    });
+
+    it("uninstalls guida package on package guida.json", () => {
+        const tmpobj = tmp.dirSync();
+
+        fs.mkdirSync(path.join(tmpobj.name, "src"));
+
+        fs.writeFileSync(path.join(tmpobj.name, "guida.json"), JSON.stringify({
+            type: "package",
+            name: "author/project",
+            summary: "helpful summary of your project, less than 80 characters",
+            license: "BSD-3-Clause",
+            version: "1.0.0",
+            "exposed-modules": [],
+            "guida-version": "1.0.0 <= v < 2.0.0",
+            dependencies: {
+                "guida-lang/stdlib": "1.0.0 <= v < 2.0.0",
+                "guida-lang/project-metadata-utils": "1.0.0 <= v < 2.0.0"
+            },
+            "test-dependencies": {}
+        }));
+
+        child_process.execSync(`${path.join(__dirname, "..", "bin", "index.js")} uninstall guida-lang/project-metadata-utils --yes`, {
+            cwd: tmpobj.name,
+            stdio: "pipe",
+            env: {
+                ...process.env,
+                GUIDA_HOME: path.join(tmpobj.name, ".guida"),
+                GUIDA_REGISTRY: "http://localhost:3210"
+            }
+        });
+
+        expect(fs.existsSync(path.join(tmpobj.name, "guida.json"))).toBe(true);
+        expect(JSON.parse(fs.readFileSync(path.join(tmpobj.name, "guida.json"), "utf-8"))).toEqual({
+            type: "package",
+            name: "author/project",
+            summary: "helpful summary of your project, less than 80 characters",
+            license: "BSD-3-Clause",
+            version: "1.0.0",
+            "exposed-modules": [],
+            "guida-version": "1.0.0 <= v < 2.0.0",
+            dependencies: {
+                "guida-lang/stdlib": "1.0.0 <= v < 2.0.0"
+            },
+            "test-dependencies": {}
+        });
+    });
 });
